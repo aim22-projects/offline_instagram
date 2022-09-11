0c73c76d1ae2391e2de43b0580e764989169d53b
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2022-09-11 12:04:35 +0530

    SettingsProvider is injected in SettingsScreen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/provider_consumer.dart';
+import 'settings_provider.dart';
+
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
+  Widget build(context) {
+    return ProviderConsumer<SettingsProvider>(
+      create: (context) => SettingsProvider(context),
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Settings'),
+        ),
       ),
     );
   }
