39fe2724619e45d48d2f9993f7466d8a16296bef
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2022-09-11 12:06:13 +0530

    PostContainer is moved to post_container folder

---
+++ lib/screens/home/post_container/post_container.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+import '../../../widgets/provider_consumer.dart';
+import 'post_provider.dart';
+
+class PostContainer extends StatelessWidget {
+  final String path;
+  const PostContainer({super.key, required this.path});
+
+  @override
+  Widget build(context) {
+    return ProviderConsumer<PostProvider>(
+      create: (context) => PostProvider(context, path),
+      builder: (context, model, child) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ListTile(
+              title: Text(basename(path)),
+            ),
+            SizedBox(
+              height: model.height,
+              child: PageView.builder(
+                controller: PageController(
+                  initialPage: 0,
+                  keepPage: false,
+                  viewportFraction: 1,
+                ),
+                physics: const ClampingScrollPhysics(),
+                scrollBehavior: const MaterialScrollBehavior(),
+                itemCount: model.media.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => InkWell(
+                  onTap: () => model.goToPostScreen(model.media[index].path),
+                  child: Image.file(
+                    model.media[index],
+                    scale: 1,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
